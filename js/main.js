864dc9fc0d54a763e188380f4405e3a7450bf5c6
Mark Lordan <mark.lordan@me.com>
2015-11-10 19:49:59 +0000

    initial commit of website files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    setTimeout(function () {
+        $('body').addClass('loaded');
+    }, 3000);
+
+
+    $("#portfolio-scroll").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".portfolio-container").offset().top
+        },
+            'slow');
+    });
+    $("#backtotop").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".header-titles").offset().top
+        },
+            'slow');
+    });
+});
