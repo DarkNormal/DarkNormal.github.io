b9f8bd78686c924c3aef1d76a52f356f57ebb5c7
DarkNormal <mark.lordan@me.com>
2015-11-16 22:05:27 +0000

    preloader

--- js/main.js
+++ js/main.js
@@ -4,13 +4,6 @@
         $('body').addClass('loaded');
     }, 3000);
 
-
-    //$("#portfolio-scroll").click(function () {
-    //    $('html,body').animate({
-    //        scrollTop: $(".portfolio-container").offset().top
-    //    },
-    //        'slow');
-    //});
     $('#myForm').validate({
         rules: {
             field1: {
@@ -45,5 +38,37 @@
             }
         });
     });
-    
+
+    $("#dropdown-group").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+
+    /* Every time the window is scrolled ... */
+    $('main').scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.hideme').each(function (i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it in */
+            if (bottom_of_window > bottom_of_object + 50) {
+
+                $(this).animate({ 'opacity': '1' }, 500);
+
+            }
+
+        });
+
+    });
 });
+   
