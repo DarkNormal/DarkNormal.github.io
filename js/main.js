46b7c8b4a3a59f0f8f9f3dceb71f0c42df952fb8
DarkNormal <mark.lordan@me.com>
2015-11-17 12:23:31 +0000

    heading image resize

--- js/main.js
+++ js/main.js
@@ -51,24 +51,24 @@
                 $('b', this).toggleClass("caret caret-up");                
             });
 
-    /* Every time the window is scrolled ... */
-    $('main').scroll(function () {
+    // /* Every time the window is scrolled ... */
+    // $('main').scroll(function () {
 
-        /* Check the location of each desired element */
-        $('.hideme').each(function (i) {
+    //     /* Check the location of each desired element */
+    //     $('.hideme').each(function (i) {
 
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+    //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* If the object is completely visible in the window, fade it in */
-            if (bottom_of_window > bottom_of_object + 50) {
+    //         /* If the object is completely visible in the window, fade it in */
+    //         if (bottom_of_window > bottom_of_object) {
 
-                $(this).animate({ 'opacity': '1' }, 500);
+    //             $(this).animate({ 'opacity': '1' }, 100);
 
-            }
+    //         }
 
-        });
+    //     });
 
-    });
+    // });
 });
    
